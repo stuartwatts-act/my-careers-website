daa1db71a489bd0e5288f8f8e38e23af5bfc86ff
stuartwatts-act <61128011+stuartwatts-act@users.noreply.github.com>
2023-12-21 01:45:28 +0000

    added api jobs

--- app.py
+++ app.py
@@ -1,39 +1,39 @@
 from os import O_NDELAY, O_RDONLY
-from flask import Flask, render_template
+from flask import Flask, json, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS = [
-  {
+JOBS = [{
     'id': 1,
     'title': 'Data Analyst',
     'location': 'Walkabout Creek',
     'salary': '$50,000'
-  },
-  {
+}, {
     'id': 2,
     'title': 'Data Engineer',
     'location': 'Wolfe Creek',
     'salary': '$40,000'
-  },
-  {
+}, {
     'id': 3,
     'title': 'Data Scientist',
     'location': 'Dunedoo',
     'salary': '$45,000'
-    
-  },
-  {
+}, {
     'id': 4,
     'title': 'Business Analyst',
     'location': 'Timbuktu ',
     'salary': '$90,000'
-  }
-]
+}]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html', jobs=JOBS, company_name = 'Bodacious ')
+  return render_template('home.html', jobs=JOBS, company_name='Bodacious ')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
