9004e8475f2b9bd8525c95ecefe6ae399543f6dc
stuartwatts-act <61128011+stuartwatts-act@users.noreply.github.com>
2023-12-27 06:22:45 +0000

    made minor updates - fomatting

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ from database import load_jobs_from_db
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
   return render_template('home.html', jobs=jobs, company_name='Bodacious')
 
+
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
