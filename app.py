cdf864e2a56124c2d7018e8d016227ca9ae94c29
stuartwatts-act <61128011+stuartwatts-act@users.noreply.github.com>
2023-12-28 06:08:44 +0000

    added dynamic job title pages - fixed typo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from os import O_NDELAY, O_RDONLY
 from flask import Flask, json, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -8,13 +8,22 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name='Bodacious')
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+
+  return render_template('jobpage.html', job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -17,3 +17,16 @@ def load_jobs_from_db():
     for row in result:
       jobs.append(row._asdict())
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),
+      {'val': id}
+    )
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
+
