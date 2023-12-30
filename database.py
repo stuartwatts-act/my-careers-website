8df5f4e9566f74242401c5cdc8f46b56701875bc
stuartwatts-act <61128011+stuartwatts-act@users.noreply.github.com>
2023-12-30 05:39:22 +0000

    added dynamic pages and thank you for applying page

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, values
 import os
 
 db_connonection_string = os.environ['DB_CONNECTION_STRING']
@@ -18,15 +18,35 @@ def load_jobs_from_db():
       jobs.append(row._asdict())
     return jobs
 
+
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(
-      text("SELECT * FROM jobs WHERE id = :val"),
-      {'val': id}
-    )
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                          {'val': id})
     rows = result.all()
     if len(rows) == 0:
       return None
     else:
       return rows[0]._asdict()
 
+
+# def add_application_to_db(job_id, data):
+#   with engine.connect() as conn:
+#     query = text(
+#         "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+#     )
+
+#     # Check if all keys in 'data' exist
+#     expected_keys = [
+#         'full_name', 'email', 'linkedin_url', 'education', 'work_experience',
+#         'resume_url'
+#     ]
+#     if not all(key in data for key in expected_keys):
+#       raise ValueError("Missing keys in data dictionary")
+
+#     # Make sure the keys match the column names exactly
+#     conn.execute(
+#         query,
+#         job_id=job_id,
+#         **data  # Pass the dictionary directly to unpack its values
+#     )
